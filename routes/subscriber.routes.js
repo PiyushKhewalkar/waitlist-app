1525b32312fa0547f3cbb1a5f5a10a483252c40c
PiyushKhewalkar <work.piyushkkr@gmail.com>
2025-05-25 16:58:16 +0530

    Add Subscriber

--- routes/subscriber.routes.js
+++ routes/subscriber.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import {getSubscribers, deleteSubscriber, exportToCSV } from "../controllers/subscribers.controller.js";
+import {getSubscribers, deleteSubscriber, exportToCSV, addSubscriber } from "../controllers/subscribers.controller.js";
 
 const subscriberRouter = Router()
 
@@ -8,5 +8,6 @@ subscriberRouter.get("/:pageId", getSubscribers)
 // subscriberRouter.get("/:id", getSubscriber)
 subscriberRouter.delete("/:id/delete", deleteSubscriber)
 subscriberRouter.post("/export", exportToCSV)
+subscriberRouter.post("/:pageId", addSubscriber)
 
 export default subscriberRouter
