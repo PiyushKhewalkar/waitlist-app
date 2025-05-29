52173a50df214de3d0d8f03f91329f9ff4c0d242
PiyushKhewalkar <work.piyushkkr@gmail.com>
2025-05-29 13:56:11 +0530

    Added delete subscriber functionality

--- routes/subscriber.routes.js
+++ routes/subscriber.routes.js
@@ -8,7 +8,7 @@ const subscriberRouter = Router()
 
 subscriberRouter.get("/:pageId", authorize, getSubscribers)
 // subscriberRouter.get("/:id", getSubscriber)
-subscriberRouter.delete("/:id/delete", authorize, deleteSubscriber)
+subscriberRouter.delete("/:id", authorize, deleteSubscriber)
 subscriberRouter.post("/export", authorize, exportToCSV)
 subscriberRouter.post("/:pageId", addSubscriber)
 
