a1f7ebbd180e7ad08198bd56f39e8d37a7f59754
PiyushKhewalkar <work.piyushkkr@gmail.com>
2025-06-09 10:25:33 +0530

    added allowed origins

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 
-const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5501"]
+const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5501", "https://hypelister.com"]
 
 app.use(cors({origin: allowedOrigins, credentials: true}))
 
