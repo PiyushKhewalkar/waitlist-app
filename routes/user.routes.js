b63b5ab252a6ca8f6fd76b8525e372e9a6b9a82a
PiyushKhewalkar <work.piyushkkr@gmail.com>
2025-06-11 10:10:19 +0530

    User route order 1

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 import { getUser, getUsers, deleteUser, getSelf } from "../controllers/user.controller.js";
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router()
 
-userRouter.get("/self", getSelf)  // More specific route first
+userRouter.get("/self", authorize, getSelf)  // More specific route first
 userRouter.get("/", getUsers)     // Then the root route
 userRouter.get("/:id", getUser)   // Then the parameterized route
 userRouter.delete("/:id", deleteUser)
