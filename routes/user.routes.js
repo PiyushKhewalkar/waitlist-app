18bb6ababccd327749c6e449f31bb7e3b29e9fec
PiyushKhewalkar <work.piyushkkr@gmail.com>
2025-06-11 10:08:46 +0530

    User route order

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,12 +3,9 @@ import { Router } from "express";
 
 const userRouter = Router()
 
-userRouter.get("/", getUsers)
-
-userRouter.get("/:id", getUser)
-
-userRouter.get("/self", getSelf)
-
+userRouter.get("/self", getSelf)  // More specific route first
+userRouter.get("/", getUsers)     // Then the root route
+userRouter.get("/:id", getUser)   // Then the parameterized route
 userRouter.delete("/:id", deleteUser)
 
 export default userRouter
